34c6aad151dfcf8607f67c5add2619ca0abd12fe
Corey Gray <16312666+csgray@users.noreply.github.com>
2025-04-03 09:43:31 -0400

    feat: Setup multiple webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,14 @@
-// webpack.config.js
+// webpack.common.js
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import {resolve} from "path";
 
 export default {
     // Required for all projects
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: resolve(process.cwd(), "dist"),
         clean: true,
-    },
-    // Source map for better debugging
-    devtool: "eval-source-map",
-    // Automatically reloads when JavaScript or watched files change
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         // Bundle HTML file
---
+++ webpack.prod.js
@@ -0,0 +1,8 @@
+// webpack.dev.js
+import {merge} from 'webpack-merge';
+import common from './webpack.common.js';
+
+export default merge(common, {
+    mode: "production",
+    devtool: "source-map",
+});
